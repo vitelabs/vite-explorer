24fce37e2caf72ee3d1007e3bd363b5d530f77ac
zhoudan <zhoudan@vite.org>
2018-06-19 11:56:02 +0800

    explorer init

---
+++ pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+</template>
+
+<script>
+    export default {
+        asyncData (ctx) {
+            return { name: 'world'}
+        },
+        data () {
+            return {
+                title: 'hahha'
+            }
+        },
+        head () {
+            return {
+                title: this.title,
+            }
+        }
+    }
+</script>
+<style lang="scss" rel="stylesheet/scss" scoped>
+
+</style>
