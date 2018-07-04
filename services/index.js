18523c9449f3923a03b1dabc745fb3aff12b339f
zhoudan <zhoudan@vite.org>
2018-07-04 11:21:24 +0800

    remove some code

--- services/index.js
+++ services/index.js
@@ -1,6 +1,5 @@
 import * as auth from "./auth";
 
-let keys = {};
 const exportObj = {};
 let services = {
   auth
@@ -8,11 +7,7 @@ let services = {
 
 Object.keys(services).forEach((item) => {
   Object.keys(services[item]).forEach((key) => {
-    if (keys[key]) {
-      throw new Error(`Service 名称重复: ${item}下的名称${key}和${keys[key]}下的${key}重复`);
-    }
     exportObj[key] = services[item][key];
-    keys[key] = item;
   });
 });
 
