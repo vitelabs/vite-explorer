ea406c0f132ae6f8d87a11c9303013f0c9b3b351
zhoudan <zhoudan@vite.org>
2018-07-25 19:58:00 +0800

    add search error

--- pages/searchError/index.vue
+++ pages/searchError/index.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="search-error">
-    <div class="error-text"> {{ $t('sError.text') }} </div>
+    <div class="error-content">
+      <img src="~assets/images/empty.svg"/>
+      <div class="error-text"> {{ $t('sError.text') }} </div>
+    </div>
   </div>
 </template>
 
@@ -31,14 +34,20 @@ export default {
     display: flex;
     display: -webkit-flex;
     height: 100%;
+    text-align: center;
+    .error-content {
+      width: 240px;
+      margin: auto;
+      margin-top: 242px; 
+    }
   }
   .error-text {
+    font-family: PingFangSC-Regular;
     margin: auto;
-    margin-top: 200px;
     color: #333;
-    font-size: 24px;
-    width: 328px;
-    padding: 20px;
-    background-color: #eee;
+    font-size: 16px;
+    color: #8D9BAE;
+    text-align: center;
+    margin-top: 30px; 
   }
 </style>
