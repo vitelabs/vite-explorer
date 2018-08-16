17c60a78014f1a507866345e798a41fb137f2e1a
zhoudan <zhoudan@vite.org>
2018-08-16 11:19:16 +0800

    bugfix hotload no cache

--- server/middleware.js
+++ server/middleware.js
@@ -3,7 +3,7 @@ import routes from "./routes";
 
 export default (app, nuxt) => {
   app.use(async (ctx, next) => {
-    if (!(/\/api\/.*/.test(ctx.path)) && ctx.path !== "https://api.coinmarketcap.com/v2/ticker/2937/") {
+    if (!(/\/api\/.*/.test(ctx.path)) && ctx.path !== "https://api.coinmarketcap.com/v2/ticker/2937/" && ctx.path !== "__webpack_hmr") {
       ctx.set("Cache-Control", "public, max-age=31536000");
     }
     await next();
