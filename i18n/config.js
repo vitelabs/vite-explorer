65bcd10acd3334144ecdf31b0402af17820ae570
zhoudan <zhoudan@vite.org>
2018-09-17 16:02:23 +0800

    bugfix i18n

--- i18n/config.js
+++ i18n/config.js
@@ -6,10 +6,10 @@ module.exports = {
     { code: "en", iso: "en-US", file: "en.js" , name: "English"},
     { code: "zh", iso: "zh-ZH", file: "zh.js" , name: "CH"}
   ],
-  detectBrowserLanguage: {
-    useCookie: true,
-    cookieKey: "i18n_redirected"
-  },
+  // detectBrowserLanguage: {
+  //   useCookie: true,
+  //   cookieKey: "i18n_redirected"
+  // },
   vueI18n: {
     fallbackLocale: defaultLocale,
     messages: {
