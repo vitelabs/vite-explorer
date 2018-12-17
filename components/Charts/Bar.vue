34b4e6ba55df045add33c1ab61f58b01e4260070
zhoudan <zhoudan@vite.org>
2018-12-17 16:01:55 +0800

    add bar dispatch color

--- components/Charts/Bar.vue
+++ components/Charts/Bar.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="bar-container">
     <div class="content">
-      <div id="main" style="width: 100%; height: 225px; margin-top: -32px;"></div>
+      <div id="main" style="width: 110%; height: 225px; margin-top: -32px; margin-left: -40px;"></div>
     </div>
   </div>
 </template>
@@ -10,18 +10,174 @@
 import echarts from "echarts";
 
 
-const colors = ["rgb(164,205,238)","rgb(42,170,227)","rgb(25,46,94)","rgb(195,229,235)"];
-
-const defaultSeriesOptions = {
-  type: "bar",
-  data: [10, 20, 30, 40, 50, 60],
-  itemStyle: {
-    color: function (params){
-      let colorList = colors;
-      return colorList[params.dataIndex];
-    }
-  }
+const data = {
+  percents: [{
+    name: "0s",
+    percent: 0.02,
+    sumPercent: 0.02
+  }, {
+    name: "0.25s",
+    percent: 0.3,
+    sumPercent: 0.32
+  }, {
+    name: "0.5s",
+    percent: 0.3,
+    sumPercent: 0.62
+  }, {
+    name: "0.75s",
+    percent: 0.3,
+    sumPercent: 0.92
+  }, {
+    name: "1s",
+    percent: 0.08,
+    sumPercent: 1.0
+  }, {
+    name: "1.25s",
+    percent: 0.1,
+    sumPercent: 0.4
+  }, {
+    name: "1.5s",
+    percent: 0.13,
+    sumPercent: 0.5
+  }, {
+    name: "1.75s",
+    percent: 0.13,
+    sumPercent: 0.5
+  }, {
+    name: "2s",
+    percent: 0.13,
+    sumPercent: 0.5
+  }, {
+    name: "2.25s",
+    percent: 0.13,
+    sumPercent: 0.5
+  }, {
+    name: "2.5s",
+    percent: 0.13,
+    sumPercent: 0.5
+  }, {
+    name: "2.75s",
+    percent: 0.13,
+    sumPercent: 0.5
+  }, {
+    name: "3s",
+    percent: 0.13,
+    sumPercent: 0.5
+  }, {
+    name: "3.25s",
+    percent: 0.13,
+    sumPercent: 0.5
+  }, {
+    name: "3.5s",
+    percent: 0.13,
+    sumPercent: 0.5
+  }, {
+    name: "3.75s",
+    percent: 0.13,
+    sumPercent: 0.5
+  }, {
+    name: "4s",
+    percent: 0.13,
+    sumPercent: 0.5
+  }, {
+    name: "4.25s",
+    percent: 0.13,
+    sumPercent: 0.5
+  }, {
+    name: "4.5s",
+    percent: 0.13,
+    sumPercent: 0.5
+  }, {
+    name: "4.75s",
+    percent: 0.13,
+    sumPercent: 0.5
+  }, {
+    name: "5s",
+    percent: 0.13,
+    sumPercent: 0.5
+  }, {
+    name: "5.25s",
+    percent: 0.02,
+    sumPercent: 0.02
+  }, {
+    name: "5.5s",
+    percent: 0.3,
+    sumPercent: 0.32
+  }, {
+    name: "5.75s",
+    percent: 0.3,
+    sumPercent: 0.62
+  }, {
+    name: "6s",
+    percent: 0.3,
+    sumPercent: 0.92
+  }, {
+    name: "6.25s",
+    percent: 0.08,
+    sumPercent: 1.0
+  }, {
+    name: "6.5s",
+    percent: 0.1,
+    sumPercent: 0.4
+  }, {
+    name: "6.75s",
+    percent: 0.13,
+    sumPercent: 0.5
+  }, {
+    name: "7s",
+    percent: 0.13,
+    sumPercent: 0.5
+  }, {
+    name: "7.25s",
+    percent: 0.13,
+    sumPercent: 0.5
+  }, {
+    name: "7.5s",
+    percent: 0.13,
+    sumPercent: 0.5
+  }, {
+    name: "7.75s",
+    percent: 0.13,
+    sumPercent: 0.5
+  }, {
+    name: "8s",
+    percent: 0.13,
+    sumPercent: 0.5
+  }, {
+    name: "8.25s",
+    percent: 0.13,
+    sumPercent: 0.5
+  }, {
+    name: "8.5s",
+    percent: 0.13,
+    sumPercent: 0.5
+  }, {
+    name: "8.75s",
+    percent: 0.13,
+    sumPercent: 0.5
+  }, {
+    name: "9s",
+    percent: 0.13,
+    sumPercent: 0.5
+  }, {
+    name: "9.25s",
+    percent: 0.13,
+    sumPercent: 0.5
+  }, {
+    name: "9.5s",
+    percent: 0.13,
+    sumPercent: 0.5
+  }, {
+    name: "9.75s",
+    percent: 0.13,
+    sumPercent: 0.5
+  }, {
+    name: "10s",
+    percent: 0.13,
+    sumPercent: 0.5
+  }]
 };
+
 
 export default {
   props: {
@@ -51,17 +207,48 @@ export default {
     clear() {
       this.echarsInstance.clear();
     },
+    dispatchBarColor(name) {
+      if (name === 0) return "#749AF8";
+      if (name > 0 && name <= 1) return "#5CB85C";
+      if (name > 1 && name <= 3) return "#F4CD41";
+      if (name > 3 && name <= 7) return "#E87400";
+      if (name > 7) return "#EE2E38";
+      return "";
+    },
     draw() {
       this.echarsInstance = echarts.init(document.getElementById("main"));
       this.echarsInstance.setOption({
         xAxis: {
           type: "category",
-          data: ["0s", "2s", "4s", "6s", "8s", "10s"]
+          data: data.percents.map(item => {
+            return item.name;
+          })
         },
         yAxis:{ 
-          position: "right"
+          position: "right",
+          axisLabel : { 
+            formatter : "{value}%" 
+          }, 
+          splitLine:{ 
+            show:false 
+          }
         },
-        series: [defaultSeriesOptions]
+        tooltip: {
+          trigger: "axis"
+        },
+        series: [{
+          type: "bar",
+          data: data.percents.map(item => {
+            return item.percent * 100;
+          }),
+          itemStyle: {
+            color: params =>{
+              let itemName = data.percents[params.dataIndex].name;
+              data.percents[params.dataIndex].name = itemName.substring(0, itemName.length - 1);
+              return this.dispatchBarColor(+data.percents[params.dataIndex].name);
+            }
+          }
+        }]
       });
     }
   }
