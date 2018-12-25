eb356c0fe6477cd7c051dce949c01a0c6295d33f
zhoudan <zhoudan@vite.org>
2018-12-25 18:01:49 +0800

    change uuid

--- server/middleware.js
+++ server/middleware.js
@@ -1,14 +1,14 @@
 import bodyParser from "koa-bodyparser";
 import routes from "./routes";
-const uuidv4 = require("uuid/v4");
-let uuid = uuidv4();
+const uuidv1 = require("uuid/v1");
+
 
 export default (app, nuxt) => {
   app.use(async (ctx, next) => {
-    // if (!(/\/api\/.*/.test(ctx.path)) && ctx.path !== "https://api.coinmarketcap.com/v2/ticker/2937/" && ctx.path !== "__webpack_hmr") {
-    //   ctx.set("Cache-Control", "public, max-age=60");
-    // }
-    ctx.cookies.set("uuid", uuid, {maxAge: 24 * 60 * 60 * 1000, httpOnly: false});
+    if (!ctx.cookies.get("uuid")) {
+      let uuid = uuidv1();
+      ctx.cookies.set("uuid", uuid, {maxAge: 24 * 60 * 60 * 1000, httpOnly: false});
+    }
     await next();
   });
 
