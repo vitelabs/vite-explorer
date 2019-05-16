80e4a0b6580138534399e0f372d802c5299c7217
zhoudan <zhoudan@vite.org>
2019-05-16 18:13:37 +0800

    f chuangshi

--- components/producerList.vue
+++ components/producerList.vue
@@ -49,11 +49,11 @@
     methods: {
       cycleToDate(cycle) {
         if (cycle === 0) {
-          return "2018-11-08 12:13:14 -- 2018-11-08 12:13:14";
+          return "2019-05-15 12:00:00 -- 2019-05-15 12:00:00";
         }
-        let date = moment((cycle * 1152 * 75 + 1541650394 - 60 * 60 * 24) * 1000).format("YYYY-MM-DD");
-        let endDate = moment((cycle * 1152 * 75 + 1541650394 ) * 1000).format("YYYY-MM-DD");
-        return  date + " 12:13:14" + " -- " + endDate +" 12:13:13";
+        let date = moment((cycle * 1152 * 75 + 1557892800 - 60 * 60 * 24) * 1000).format("YYYY-MM-DD");
+        let endDate = moment((cycle * 1152 * 75 + 1557892800 ) * 1000).format("YYYY-MM-DD");
+        return  date + " 12:00:00" + " -- " + endDate +" 11:59:59";
       },
     }
   };
