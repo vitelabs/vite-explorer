df3c0abb3f116bb76bca8fdead221f0a1c780388
zhoudan <zhoudan@vite.org>
2019-05-21 13:42:35 +0800

    change proxy url

--- server/config/production.js
+++ server/config/production.js
@@ -1,7 +1,7 @@
 
 
 module.exports =  {
-  explorerApi: "http://172.19.0.30:8080/api",
-  sbpDetailApi: "http://150.109.62.152:8080",
+  explorerApi: "http://172.19.0.12:8080/api",
+  sbpDetailApi: "http://124.156.104.194:8080",
   statsApi: "https://stats.vite.net/apitest"
 };
