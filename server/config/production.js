2852ae0105011a134fd6264c8c78d2f0980e2e70
jerry-vite <yinjun@vite.org>
2019-12-23 16:05:05 +0800

    change backend ip

--- server/config/production.js
+++ server/config/production.js
@@ -1,7 +1,7 @@
 
 
 module.exports =  {
-  explorerApi: "http://172.19.0.12:8080/api",
+  explorerApi: "http://172.19.0.10:8080/api",
   sbpDetailApi: "http://124.156.104.194:8080",
   statsApi: "https://stats.vite.net/apitest"
 };
